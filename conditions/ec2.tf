d70a89d426ee21ddc287ac0aa35888dbb7a3e6ec
Naveen <guvvala4835@gmail.com>
2025-10-20 15:06:57 +0530

    conditions

--- conditions/ec2.tf
+++ conditions/ec2.tf
@@ -1,7 +1,33 @@
-resource "aws_instance" "ec2"{
-    ami  = "ami-0199d4b5b8b4fde0e"
-    instance_type = "t2.micro"
-    region = "us-east-2"
+resource "aws_instance" "terraform" {
+    ami = "ami-09c813fb71547fc4f"
+    instance_type = var.environment == "dev" ? "t3.micro" : "t3.medium"
+    vpc_security_group_ids = [aws_security_group.allow_all.id]
+    tags = {
+        Name = "terraform"
+        Terraform = "true"
+    }
 }
 
+resource "aws_security_group" "allow_all" {
+  name   = "allow-all"
+
+  egress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  ingress {
+    from_port        = 0 # from port 0 to to port 0 means all ports
+    to_port          = 0 
+    protocol         = "-1" # -1 means all protocols
+    cidr_blocks      = ["0.0.0.0/0"] # internet
+  }
+
+  tags = {
+    Name = "allow-all"
+  }
+
+}
 #cd ~/DevOps/daws-86s/repos/Terraform
--- conditions/variables.tf
+++ conditions/variables.tf
@@ -1,8 +1,3 @@
-variable "ami_id"{
-    type = string
-    default = "ami-0199d4b5b8b4fde0e"
-}
-variable "instnace_type"{
-    type = string
-    default = "t2.micro"
+variable "environment" {
+    default = "qa"
 }
